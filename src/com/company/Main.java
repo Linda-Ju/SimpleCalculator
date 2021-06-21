012776ac30c6905b4cc9e1444cdf73e65480d15d
Linda-Ju <linda.junkina@gmail.com>
2021-06-21 21:47:39 +0300

    Simple Calculator (homework, operators, loops, Math.pow, Math.max, Math min)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,15 +13,75 @@ public class Main {
 
         String operator = "/";
 
-        if (operator.equals("+")){
+        if (operator.equals("+")) {
             answer = firstNumber + secondNumber;
-        }else if (operator.equals("-")){
+        } else if (operator.equals("-")) {
             answer = firstNumber - secondNumber;
-        }else if (operator.equals("*")){
+        } else if (operator.equals("*")) {
             answer = firstNumber * secondNumber;
-        }else if (operator.equals("/")) {
+        } else if (operator.equals("/")) {
             answer = firstNumber / secondNumber;
         }
-        System.out.println(answer);
+//        % - modulo;
+//        5 % 2 = 1
+
+
+//        System.out.println(answer);
+//        int five = 5;
+
+//        System.out.println();
+
+//        && - and
+//        || - or
+//        ! - not
+//        != - not equal
+//        == equal
+//        >= greater than or equal
+//        <= less than  or equal
+
+//        if (6 < 8 != 5 > 9) {
+//            System.out.println("EQUALS works");
+//        } else {
+//            System.out.println("EQUAL skipped");
+//        }
+
+
+//      LOOPS
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("value of i: " + i);
+//        }
+//        for (int i = 1; i <= 10; i++) {
+//            if (i >= 6) {
+//                System.out.println("i = " + i);
+//            }
+//        }
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println("i even = " + i);
+//            } else {
+//                System.out.println("i odd= " + i);
+//            }
+//        }
+
+//        int counter = 1;
+//        while (counter <= 10) {
+//            System.out.println("counter = " + counter);
+//            counter++;
+//        }
+//
+//        do {
+//            System.out.println("counter = " + counter);
+//            counter++;
+//        } while (counter <= 10);
+
+
+//        Math.pow
+//        Math.max
+//        Math min
+        System.out.println(Math.max(5, 4));
+        
+        int minimum = Math.min(5, 200);
+        System.out.println("minimum = " + minimum);
+
     }
 }
